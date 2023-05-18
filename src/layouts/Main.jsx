9dcb294d193a6dc4261eeeb70e81586a77e99d62
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-05-18 23:35:09 +0600

    Added loading animation

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,8 +1,11 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../pages/Shared/Header/Header";
 import { useEffect } from "react";
+import Lottie from "lottie-react";
+import loadingAnim from "../assets/common/loading.json";
 const Main = () => {
   const location = useLocation();
+  const navigation = useNavigation();
   useEffect(() => {
     if (location.pathname == "/") {
       document.title = `Toyzz haven | Home`;
@@ -10,10 +13,19 @@ const Main = () => {
       document.title = `Toyzz haven | ${location.pathname.replace("/", "")}`;
     }
   }, [location]);
-  console.log(location);
+
   return (
     <>
       <Header />
+      {navigation.state === "loading" && (
+        <div className="w-full min-h-screen bg-white fixed top-0 left-0 flex justify-center items-center z-40">
+          <Lottie
+            animationData={loadingAnim}
+            loop={true}
+            className="w-56 h-56"
+          />
+        </div>
+      )}
       <Outlet />
     </>
   );
