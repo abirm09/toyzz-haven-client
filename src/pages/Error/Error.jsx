20398fd0e14df7381a9de4c56b476e1f710ec1ab
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-05-19 10:07:06 +0600

    implemented toy details page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+import errorImg from "../../assets/Error-page/page-404.svg";
 const Error = () => {
   return (
-    <div>
-      <h3>Opps....</h3>
-    </div>
+    <section>
+      <div className="space-y-10">
+        <img className="max-w-lg mx-auto mt-20" src={errorImg} alt="404" />
+        <div className="flex justify-center">
+          <Link to="/" className="cs-btn-primary ">
+            Back to home
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/pages/ToyDetails/ToyDetails.jsx
+++ src/pages/ToyDetails/ToyDetails.jsx
@@ -1,5 +1,6 @@
+import { Rating } from "@smastrom/react-rating";
 import { useEffect } from "react";
-import { useLoaderData, useLocation } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 const ToyDetails = () => {
   const data = useLoaderData();
@@ -7,10 +8,49 @@ const ToyDetails = () => {
     document.title = `Toyzz haven | ${data?.toy_name}`;
   }, [data]);
   console.log(data);
+  const {
+    toy_name,
+    toy_pic,
+    email,
+    price,
+    rating,
+    available_quantity,
+    short_description,
+    seller_name,
+  } = data;
   return (
-    <div>
-      <h3>fkl</h3>
-    </div>
+    <section className="cs-container my-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center">
+        <div>
+          <img src={toy_pic} alt="" />
+        </div>
+        <div>
+          <h2 className="text-3xl font-poppins font-semibold">{toy_name}</h2>
+          <div className="font-poppins text-sm space-y-3 mt-5 text-slate-600">
+            <p>
+              Seller : <span>{seller_name}</span>
+            </p>
+            <p>
+              Email : <span> {email}</span>
+            </p>
+            <p>Available quantity : {available_quantity}</p>
+            <h3 className="text-3xl font-semibold cs-text-gradient">
+              $ {price} <span className="text-base">tax incl.</span>
+            </h3>
+            <div>
+              <Rating
+                style={{ maxWidth: 85 }}
+                value={Math.round(rating)}
+                readOnly
+              />
+            </div>
+            <p className="text-slate-500 font-poppins font-semibold">
+              {short_description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
